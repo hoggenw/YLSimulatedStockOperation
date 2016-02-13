7f1e14c987aa645ff9cbf3018cf0a99931108a0a
Waterstrong <sqlin@thoughtworks.com>
2016-02-13 21:28:28 +0800

    模拟界面ui，买入ui完成

---
+++ YLSimulatedStockOperation/YLSIghnStockMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  YLSIghnStockMessageViewController.h
+//  YLSimulatedStockOperation
+//
+//  Created by Waterstrong on 2/13/16.
+//  Copyright © 2016 hoggenWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YLSIghnStockMessageViewController : UIViewController
+
+@end
---
+++ YLSimulatedStockOperation/YLSimulatedBuyViewController.h
@@ -0,0 +1,13 @@
+//
+//  YLSimulatedBuyViewController.h
+//  YLSimulatedStockOperation
+//
+//  Created by Waterstrong on 2/13/16.
+//  Copyright © 2016 hoggenWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YLSimulatedBuyViewController : UIViewController
+
+@end
